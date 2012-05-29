c31e69be1b639e295aaeafec596aa61c269cabc2
Călin Ardelean <calinucs@gmail.com>
2012-05-29 21:17:20 +0000

    added json file ~1.3MB, much faster loading

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,101 +1,87 @@
 //"use strict";
 
-var alliances = [];
-var players = [];
-var towns = [];
-var ctx, bg_img = new Image();
-var img_loaded = false, al_loaded = false, pl_loaded = false, to_loaded = false;
+var data = { alliances: [], towns: [] };
+var bg_img = new Image();
 
 $(document).ready(function () {
-  ctx = $("#map")[0].getContext("2d");
-  
+  var img_loaded = false, data_loaded = false;
   bg_img.onload = function() {
     img_loaded = true;
-    data_loaded();
+    if (data_loaded) paint();
   };
   bg_img.src="images/region_faction_map.gif";
-  
-  //load alliances from xml
-  
+  $.getJSON('data/data.json', function(d) {
+    data = d;
+    data_loaded = true;
+    if (img_loaded) paint();
+  });
+});
+
+function loadXml() {
+  var al_loaded = false, to_loaded = false;
+  var generateJson = function() {
+    var div = $("#jsondiv");
+    var json_text = JSON.stringify(data, null, 2);
+    div.show();
+    div.append(json_text);
+  }
   $.ajax({
     type: "GET",
     url: "data/datafile_alliances.xml",
     dataType: "xml",
     success: function(xml) {
-      $(xml).find("alliance").each(function() {
+      $(xml).find("alliances").children("alliance").each(function() {
         var a = {}, xml_a = $(this);
-        a.id = xml_a.find("alliance").attr("id");
-        a.capital = xml_a.find("alliancecapitaltownid").attr("id");
-        a.ticker = xml_a.find("allianceticker").text();
-        a.founded = xml_a.find("foundeddatetime").text();
-        a.members = xml_a.find("membercount").text();
-        a.population = xml_a.find("totalpopulation").text();
-        a.NAPs = [];
-        a.confeds = [];
+        a.id = parseInt(xml_a.children("alliance").attr("id"));
+        a.name = xml_a.children("alliance").text();
+        a.tck = xml_a.children("allianceticker").text();
+        var mem = parseInt(xml_a.children("membercount").text());
+        a.NAP = [];
+        a.conf = [];
         xml_a.find("relationship").each(function () {
           var xml_rel = $(this);
-          var t = xml_rel.find("relationshiptype").text();
-          var al_id = xml_rel.find("proposedbyalliance").attr("id");
-          if (al_id == a.id) al_id = xml_rel.find("acceptedbyalliance").attr("id");
-          if (t == "NAP") a.NAPs.push(al_id);
-          else if (t == "Confederation") a.confeds.push(al_id);
+          var t = xml_rel.children("relationshiptype").text();
+          var al_id = xml_rel.children("proposedbyalliance").attr("id");
+          if (al_id == a.id) al_id = xml_rel.children("acceptedbyalliance").attr("id");
+          if (t == "NAP") a.NAP.push(parseInt(al_id));
+          else if (t == "Confederation") a.conf.push(parseInt(al_id));
         });
-        alliances.push(a);
+        if (mem > 0) data.alliances.push(a);
       });
       al_loaded = true;
-      data_loaded();
+      if (to_loaded) generateJson();
     }
   });
-  
-  //load players from xml
-  
-  $.ajax({
-    type: "GET",
-    url: "data/datafile_players.xml",
-    dataType: "xml",
-    success: function(xml) {
-      $(xml).find("player").each(function() {
-        var p = {}, xml_p = $(this);
-        p.id = xml_p.find("playername").attr("id");
-        p.name = xml_p.find("playername").text();
-        p.alliance = xml_p.find("allianceid").attr("id");
-        p.race = xml_p.find("race").attr("id");
-        players.push(p);
-      });
-      pl_loaded = true;
-      data_loaded();
-    }
-  });
-  
-  //load towns from xml
   
   $.ajax({
     type: "GET",
     url: "data/datafile_towns.xml",
     dataType: "xml",
     success: function(xml) {
-      $(xml).find("town").each(function() {
+      $(xml).children("towns").children("town").each(function() {
         var t = {}, xml_t = $(this);
-        var loc = xml_t.find("location"), pl = xml_t.find("player"), dat = xml_t.find("towndata");
-        t.id = dat.find("townname").attr("id");
-        t.name = dat.find("townname").text();
-        t.population = dat.find("population").text();
-        t.capital = (dat.find("iscapitalcity").text() == "1");
-        t.al_capital = (dat.find("isalliancecapitalcity").text() == "1");
-        t.x = loc.find("mapx").text();
-        t.y = loc.find("mapy").text();
-        t.player = pl.find("playername").attr("id");
-        t.alliance = pl.find("playeralliance").find("alliancename").attr("id");
-        towns.push(t);
+        var loc = xml_t.children("location"), pl = xml_t.children("player"), dat = xml_t.children("towndata");
+        t.p = parseInt(dat.children("population").text());
+        var cap = (dat.children("iscapitalcity").text() == "1");
+        var acap = (dat.children("isalliancecapitalcity").text() == "1");
+        var capital = (cap && acap ? 3 : (acap ? 2 : (cap ? 1 : 0)));
+        if (capital > 0) t.c = capital;
+        t.x = parseInt(loc.children("mapx").text());
+        t.y = parseInt(loc.children("mapy").text());
+        var alliance = parseInt(pl.children("playeralliance").children("alliancename").attr("id"));
+        if (alliance) t.a = alliance;
+        var race = pl.children("playerrace").text().substring(0, 1);
+        if (race != "H") t.r= race;
+        if (t.p > 0) data.towns.push(t);
       });
       to_loaded = true;
-      data_loaded();
+      if (al_loaded) generateJson();
     }
   });
-});
+}
 
-function data_loaded() {
-  if (!img_loaded || !al_loaded || !pl_loaded || !to_loaded) return;
+function paint() {
+  var ctx = $("#map")[0].getContext("2d");
   ctx.drawImage(bg_img, 0, 0, 1000, 1000);
   ctx.globalAlpha = 0.8;
   ctx.beginPath();
@@ -105,7 +91,6 @@ function data_loaded() {
   ctx.globalAlpha = 1;
   ctx.font = "italic 40pt Calibri";
   ctx.fillStyle = "white";
-  ctx.fillText("Number of alliances: " + alliances.length, 150, 100);
-  ctx.fillText("Number of players: " + players.length, 150, 150);
-  ctx.fillText("Number of towns: " + towns.length, 150, 200);
+  ctx.fillText("Number of alliances: " + data.alliances.length, 150, 100);
+  ctx.fillText("Number of towns: " + data.towns.length, 150, 150);
 }
