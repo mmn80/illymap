38fb4c67a87d18de4621d7ab3f9c2bc52e69e15a
Călin Ardelean <calinucs@gmail.com>
2012-05-30 22:19:10 +0000

    show capitals ck, capital pop proportional to the circle area

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,8 @@
 //"use strict";
 
+var TOWNS_XML = "data/datafile_towns.xml";
+var ALLIANCES_XML = "data/datafile_alliances";
+
 var data = { server: "", date: "", alliances: [], towns: [] };
 var bg_img = new Image();
 var capitals = [];
@@ -16,6 +19,7 @@ $(document).ready(function () {
     if (data_loaded) initialize();
   };
   $("#show_towns").click(paint);
+  $("#show_capitals").click(paint);
   $("#xml2json_btn").click(loadXml);
   $("#map").mousemove(map_mousemove);
   bg_img.src="images/region_faction_map.gif";
@@ -25,76 +29,6 @@ $(document).ready(function () {
     if (img_loaded) initialize();
   });
 });
-
-function loadXml() {
-  var al_loaded = false, to_loaded = false;
-  var data = { server: "", date: "", alliances: [], towns: [] };
-  
-  var generateJson = function() {
-    var div = $("#jsondiv");
-    var json_text = JSON.stringify(data, null, 2);
-    div.show();
-    div.append(json_text);
-  }
-  
-  $.ajax({
-    type: "GET",
-    url: "data/datafile_alliances.xml",
-    dataType: "xml",
-    success: function(xml) {
-      $(xml).find("alliances").children("alliance").each(function() {
-        var a = {}, xml_a = $(this);
-        a.id = parseInt(xml_a.children("alliance").attr("id"));
-        a.name = xml_a.children("alliance").text();
-        a.tck = xml_a.children("allianceticker").text();
-        var mem = parseInt(xml_a.children("membercount").text());
-        a.NAP = [];
-        a.conf = [];
-        xml_a.find("relationship").each(function () {
-          var xml_rel = $(this);
-          var t = xml_rel.children("relationshiptype").text();
-          var al_id = xml_rel.children("proposedbyalliance").attr("id");
-          if (al_id == a.id) al_id = xml_rel.children("acceptedbyalliance").attr("id");
-          if (t == "NAP") a.NAP.push(parseInt(al_id));
-          else if (t == "Confederation") a.conf.push(parseInt(al_id));
-        });
-        if (mem > 0) data.alliances.push(a);
-      });
-      al_loaded = true;
-      if (to_loaded) generateJson();
-    }
-  });
-  
-  $.ajax({
-    type: "GET",
-    url: "data/datafile_towns.xml",
-    dataType: "xml",
-    success: function(xml) {
-      var server = $(xml).children("towns").children("server");
-      data.server = server.children("name").text();
-      data.date = server.children("datagenerationdatetime").text();
-      $(xml).children("towns").children("town").each(function() {
-        var t = {}, xml_t = $(this);
-        var loc = xml_t.children("location"), pl = xml_t.children("player"), dat = xml_t.children("towndata");
-        t.p = parseInt(dat.children("population").text());
-        if (dat.children("isalliancecapitalcity").text() == "1") {
-          t.c = 1;
-          t.pl = pl.children("playername").text();
-          t.name = dat.children("townname").text();
-        }
-        t.x = parseInt(loc.children("mapx").text());
-        t.y = parseInt(loc.children("mapy").text());
-        var alliance = parseInt(pl.children("playeralliance").children("alliancename").attr("id"));
-        if (alliance) t.a = alliance;
-        var race = pl.children("playerrace").text().substring(0, 1);
-        if (race != "H") t.r= race;
-        if (t.p > 0) data.towns.push(t);
-      });
-      to_loaded = true;
-      if (al_loaded) generateJson();
-    }
-  });
-}
 
 function initialize() {
   $("#server_info").html("server: " + data.server + "<br/>date: " + data.date);
@@ -123,6 +57,9 @@ function initialize() {
 
 function paint() {
   var ctx = $("#map")[0].getContext("2d");
+  
+  //paint background image
+  
   ctx.drawImage(bg_img, 0, 0, 1000, 1100);
   ctx.globalAlpha = 0.8;
   ctx.beginPath();
@@ -130,18 +67,29 @@ function paint() {
   ctx.fillStyle = "black";
   ctx.fill();
   ctx.globalAlpha = 1;
-  var imgd = ctx.getImageData(0, 0, 1000, 1100);
+  
+  //paint towns
+  
+  var imgd = ctx.getImageData(0, 0, 1000, 1000);
   if ($("#show_towns").is(':checked'))
     for (var i=0; i<data.towns.length; i++) {
       var town = data.towns[i];
-      if (town.c != 1)
-        point(imgd, town.x1, town.y1, 0, 0, 255, 255);
+      if (town.c != 1) {
+        var idx = (town.x1 + (town.y1 * imgd.width)) * 4;        
+        imgd.data[idx] = 0;
+        imgd.data[idx+1] = 0;
+        imgd.data[idx+2] = 255;
+        imgd.data[idx+3] = 255;
+      }
     }
   ctx.putImageData(imgd, 0, 0);
-  if ($("#show_towns").is(':checked'))
+  
+  //paint capitals
+  
+  if ($("#show_capitals").is(':checked'))
     for (var i=0; i<capitals.length; i++) {
       var town = capitals[i];
-      var r = Math.floor(town.p / 2500);
+      var r = Math.floor(Math.sqrt(town.p / 300));
       if (r < 2) r = 2;
       ctx.beginPath();
       var grd = ctx.createRadialGradient(town.x1, town.y1, 1, town.x1, town.y1, r);
@@ -151,6 +99,9 @@ function paint() {
       ctx.arc(town.x1, town.y1, r, 0, Math.PI*2, false);
       ctx.fill();
     }
+    
+  //paint selected capital infobox
+  
   if (map_state.sel_cap)
     info_box(ctx, map_state.sel_cap.x1 + 20, map_state.sel_cap.y1 - 10, [
       { text: map_state.sel_cap.name, italic: true },
@@ -164,10 +115,10 @@ function map_mousemove(event) {
   map_state.sel_cap = null;
   map_state.mx = event.pageX - this.offsetLeft;
   map_state.my = event.pageY - this.offsetTop;
-  if ($("#show_towns").is(':checked'))
+  if ($("#show_capitals").is(':checked'))
     for (var i=0; i<capitals.length; i++) {
         var town = capitals[i];
-        var r = Math.floor(town.p / 2500);
+        var r = Math.floor(Math.sqrt(town.p / 300));
         if (r < 2) r = 2;
         var dist = Math.sqrt(Math.pow(town.x1 - map_state.mx, 2) + Math.pow(town.y1 - map_state.my, 2));
         if (dist <= r) {
@@ -208,7 +159,7 @@ function info_box(ctx, x, y, lines) {
   
   //draw box
   
-  ctx.globalAlpha = 0.3;
+  ctx.globalAlpha = 0.4;
   ctx.beginPath();
   ctx.moveTo(x + r, y);
   ctx.lineTo(x + w - r, y);
@@ -236,10 +187,72 @@ function info_box(ctx, x, y, lines) {
   }
 }
 
-function point(imgd, x, y, r, g, b, a) {            
-  var idx = (x + (y * imgd.width)) * 4;        
-  imgd.data[idx] = r;
-  imgd.data[idx+1] = g;
-  imgd.data[idx+2] = b;
-  imgd.data[idx+3] = a;
+function loadXml() {
+  var al_loaded = false, to_loaded = false;
+  var data = { server: "", date: "", alliances: [], towns: [] };
+  
+  var generateJson = function() {
+    var div = $("#jsondiv");
+    var json_text = JSON.stringify(data, null, 2);
+    div.show();
+    div.append(json_text);
+  }
+  
+  $.ajax({
+    type: "GET",
+    url: ALLIANCES_XML,
+    dataType: "xml",
+    success: function(xml) {
+      $(xml).find("alliances").children("alliance").each(function() {
+        var a = {}, xml_a = $(this);
+        a.id = parseInt(xml_a.children("alliance").attr("id"));
+        a.name = xml_a.children("alliance").text();
+        a.tck = xml_a.children("allianceticker").text();
+        var mem = parseInt(xml_a.children("membercount").text());
+        a.NAP = [];
+        a.conf = [];
+        xml_a.find("relationship").each(function () {
+          var xml_rel = $(this);
+          var t = xml_rel.children("relationshiptype").text();
+          var al_id = xml_rel.children("proposedbyalliance").attr("id");
+          if (al_id == a.id) al_id = xml_rel.children("acceptedbyalliance").attr("id");
+          if (t == "NAP") a.NAP.push(parseInt(al_id));
+          else if (t == "Confederation") a.conf.push(parseInt(al_id));
+        });
+        if (mem > 0) data.alliances.push(a);
+      });
+      al_loaded = true;
+      if (to_loaded) generateJson();
+    }
+  });
+  
+  $.ajax({
+    type: "GET",
+    url: TOWNS_XML,
+    dataType: "xml",
+    success: function(xml) {
+      var server = $(xml).children("towns").children("server");
+      data.server = server.children("name").text();
+      data.date = server.children("datagenerationdatetime").text();
+      $(xml).children("towns").children("town").each(function() {
+        var t = {}, xml_t = $(this);
+        var loc = xml_t.children("location"), pl = xml_t.children("player"), dat = xml_t.children("towndata");
+        t.p = parseInt(dat.children("population").text());
+        if (dat.children("isalliancecapitalcity").text() == "1") {
+          t.c = 1;
+          t.pl = pl.children("playername").text();
+          t.name = dat.children("townname").text();
+        }
+        t.x = parseInt(loc.children("mapx").text());
+        t.y = parseInt(loc.children("mapy").text());
+        var alliance = parseInt(pl.children("playeralliance").children("alliancename").attr("id"));
+        if (alliance) t.a = alliance;
+        var race = pl.children("playerrace").text().substring(0, 1);
+        if (race != "H") t.r= race;
+        if (t.p > 0) data.towns.push(t);
+      });
+      to_loaded = true;
+      if (al_loaded) generateJson();
+    }
+  });
 }
